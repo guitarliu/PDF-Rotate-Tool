c2c1b2d26a71b8d35aa29653810f70b42b6cad83
guitarliu <whuliuxiaoyong@163.com>
2023-08-17 14:38:16 +0800

    新增打开文件夹功能

--- PDF-Rotate-Tool/MainWindow.xaml.cs
+++ PDF-Rotate-Tool/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PDF_Rotate_Tool
 {
@@ -86,5 +76,29 @@ namespace PDF_Rotate_Tool
             }
         }
 
+        private void PDFList_Click(object sender, RoutedEventArgs e)
+        {
+            TbkAnnotion.Visibility = Visibility.Hidden;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.Filter = "PDF Files|*.pdf";
+            if (dialog.ShowDialog() == true)
+            {
+                string[] files = dialog.FileNames;
+                // open folder etc.
+                foreach (string file in files)
+                {
+                    if (System.IO.Path.GetExtension(file).Equals(".pdf", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (!PDFslbx.Items.Contains(file))
+                        {
+                            // Add the PDF file path to ListBoxItem
+                            PDFslbx.Items.Add(file);
+                        }
+                    }
+                }
+
+            }
+        }
     }
 }
