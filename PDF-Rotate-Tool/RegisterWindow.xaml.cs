a090adcb8bf3f3918872efe0b5d1f36c28781a89
guitarliu <whuliuxiaoyong@163.com>
2023-09-10 00:44:44 +0800

    新增离线注册函数

--- PDF-Rotate-Tool/RegisterWindow.xaml.cs
+++ PDF-Rotate-Tool/RegisterWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,12 +35,25 @@ namespace PDF_Rotate_Tool
                 //throw;
             }
         }
-        private void Bt_Minimum_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
 
         private void Bt_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Bt_How_Register_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.notion.so/55a9daf124ea47b18d0322aca011d4dd?pvs=4") { UseShellExecute = true });
+        }
+
+        private void Bt_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            // Check whether Register Code is matched MachineID
+            // True, Close this window
+            // False, Show Alert MessageBox
+        }
+
+        private void Bt_Cancel_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
